f106ad8ee17fa92e14f50edaa6c2b620095cbe30
Chuck Belcher <cfbelcher@pateogroup.com>
2023-08-27 19:43:40 -0400

    Initial Commit

---
+++ TimeConverter/TimeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConverterApp.swift
+//  TimeConverter
+//
+//  Created by Chuck Belcher on 8/27/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
