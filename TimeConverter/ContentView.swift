5eb586086988eb096ee0b9f0416a187f3c1fc3fd
Chuck Belcher <cfbelcher@pateogroup.com>
2023-08-27 20:13:44 -0400

    Initial commit, stubbed out the UI. Still need to work on conversion logic

--- TimeConverter/ContentView.swift
+++ TimeConverter/ContentView.swift
@@ -4,18 +4,73 @@
 //
 //  Created by Chuck Belcher on 8/27/23.
 //
+// Challenge
+// Time conversion: users choose seconds, minutes, hours, or days.
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var userInput = 0
+    @State private var userInputUnit = "Days"
+    @State private var userOutputUnit = "Seconds"
+    @FocusState private var userInputIsFocused: Bool
+    
+    let userOptions = ["Days", "Hours", "Minutes", "Seconds"]
+    var conversionValue: Int {
+        //Compute conversion amount here
+        return 0
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    HStack {
+                        TextField("Initial Value", value: $userInput, format: .number)
+                            .keyboardType(.decimalPad)
+                            .focused($userInputIsFocused)
+                        Picker("", selection: $userInputUnit) {
+                            ForEach(userOptions, id: \.self) { unit in
+                                Text(unit)
+                            }
+                        }
+                    }
+                    
+                } header: {
+                Text("Enter initial value")
+                }
+                
+                Section {
+                    Picker("Input Unit", selection: $userOutputUnit) {
+                        ForEach(userOptions, id: \.self) { unit in
+                            Text(unit)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Select Output Unity")
+                }
+                
+                Section {
+                    HStack {
+                        Text("\(conversionValue)")
+                        Text(userOutputUnit)
+                    }
+                    .font(.title)
+                }
+            }
+            .navigationTitle("Time Conversion")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        userInputIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
+        
     }
 }
 
